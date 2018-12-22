b41a95e83310955e551dc214557e383836dfa684
Davbjorn <david.timgren@gmail.com>
2018-12-22 18:19:52 +0100

    Add project files.

---
+++ Testgrej/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testgrej.Startup))]
+namespace Testgrej
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
